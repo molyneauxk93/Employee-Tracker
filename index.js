633827a56752d7f35138adb10c8cd801cea009f9
Josh Mol <joshua.molyneaux@live.com>
2023-04-18 00:19:52 -0400

    Adding updates to function logic

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 
 //importing generateMArkdown javascript file 
-const { allDepartments, allRoles, allEmployees, addDepartment, addNewRole } = require('./queries/query.js');
+const { allDepartments, allRoles, allEmployees, addDepartment, addNewRole, addNewEmployee } = require('./queries/query.js');
+
 
 const question = [
     {
@@ -10,35 +11,10 @@ const question = [
         name: 'choice',
         choices: ['View all Departments', 'View all roles', 'View all employees', 'Add Department', 'Add role', 'Add employee', 'Update employee']
     },
-    {
-        type: 'input',
-        message: 'What is the name of the department?',
-        name: 'dpt_name',
-        when: (answer) => answer.choice === 'Add Department'
-    },
-    {
-        type: 'input',
-        message: 'What is the name of the role?',
-        name: 'new_role',
-        when: (answer) => answer.choice === 'Add role'
-    },
-    {
-        type: 'input',
-        message: 'What is the salary of the role?',
-        name: 'role_salary',
-        when: (answer) => answer.choice === 'Add role'
-    },
-    {
-        type: 'input',
-        message: 'Which department does the role belong to?',
-        name: 'role_dpt',
-        when: (answer) => answer.choice === 'Add role'
-    },
 ];
 
 async function init() {
 
-    
     const data = await inquirer.prompt(question);
 
     if(data.choice === 'View all Departments') {
@@ -47,14 +23,14 @@ async function init() {
         allRoles();
     } else if (data.choice === 'View all employees') {
         allEmployees();
-    } else if (data.dpt_name) {
-        addDepartment(data);
-    } else if(data.role_dpt) {
-        addNewRole(data);
+    } else if (data.choice === 'Add Department') {
+        addDepartment();
+    } else if(data.choice  === 'Add role') {
+        addNewRole();
+    } else if (data.choice === 'Add employee'){
+        addNewEmployee();
     }
 
 }
 
 init();
-
-// module.exports = init;
--- queries/query.js
+++ queries/query.js
@@ -1,75 +1,172 @@
-// Import and require mysql2
+// Import and require mysql2/promise
 const mysql = require('mysql2/promise');
 
-// const index = require('../index.js');
+const inquirer = require('inquirer');
 
+//function to view all departments 
 allDepartments = async () => {
 
     //create db connection
     const db = await mysql.createConnection({ host: 'localhost', user: 'root', password: '4613749J', database: 'employee_db' });
 
+    //run select query and console table results
     const results = await db.query('SELECT id, dpt_name AS Department FROM department');
 
     await console.table([...results[0]]);
 
-    // return index.init;
 }
 
+//function to view  all roles 
 allRoles = async () => {
 
     //create db connection
     const db = await mysql.createConnection({ host: 'localhost', user: 'root', password: '4613749J', database: 'employee_db' });
 
+    //run select query and console table results
     const results = await db.query('SELECT * FROM role');
 
     await console.table([...results[0]]);
 }
 
+//function to view all employees 
 allEmployees = async () => {
 
     //create db connection
     const db = await mysql.createConnection({ host: 'localhost', user: 'root', password: '4613749J', database: 'employee_db' });
 
+    //run select query and console table results 
     const results = await db.query('SELECT * FROM employee');
 
     await console.table([...results[0]]);
 }
 
-addDepartment = async (data) => {
+//function to add a new department 
+addDepartment = async () => {
 
+    const questions = [
+        {
+            type: 'input',
+            message: 'What is the name of the department?',
+            name: 'dpt_name',
+        },
+    ];
+
+    //prompt questions 
+    const data = await inquirer.prompt(questions);
+
+    //sql query and params 
     const sql = `INSERT INTO department (dpt_name) VALUES (?)`;
     const params = [data.dpt_name];
 
     //create db connection
     const db = await mysql.createConnection({ host: 'localhost', user: 'root', password: '4613749J', database: 'employee_db' });
 
+    //runs INSERT query on database amd logs success message to console 
     await db.query(sql, params);
 
     await console.log(`Added ${data.dpt_name} to the database`);
 }
 
-addNewRole = async (data) => {
-
-    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+//function to add a new role 
+addNewRole = async () => {
 
     //create db connection
     const db = await mysql.createConnection({ host: 'localhost', user: 'root', password: '4613749J', database: 'employee_db' });
 
-    const department_id = await db.query(`SELECT id FROM department WHERE dpt_name = ?`, data.role_dpt);
+    //gets is and department name from department tableand then converts json to appropriate format using .map()
+    const results = await db.query('SELECT id, dpt_name FROM department');
+    
+    const list_dpts = await results[0].map(({ id, dpt_name }) => ({ name: dpt_name, value: id }));
 
-    let dpt_result = JSON.stringify(department_id[0]);
 
-    foundId = dpt_result.split(/[{":}]/);
+    const questions = [
+        {
+            type: 'input',
+            message: 'What is the name of the role?',
+            name: 'new_role',
+        },
+        {
+            type: 'input',
+            message: 'What is the salary of the role?',
+            name: 'role_salary',
+        },
+        {
+            type: 'list',
+            message: 'Which department does the role belong to?',
+            name: 'role_dpt',
+            choices: list_dpts,
+        },
+    ]
 
-    const params = await [data.new_role, data.role_salary, foundId[4]];
+    //prompt questions 
+    const data = await inquirer.prompt(questions);
 
+    const sql = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+
+    //Converting department id from user's choice to string 
+    user_choice = data.role_dpt.toString();
+
+    const params = await [data.new_role, data.role_salary, user_choice];
+
+    //runs INSERT query and logs success message to console 
     await db.query(sql, params);
 
     await console.log(`Added ${data.new_role} to the database`);
 }
 
-addNewEmployee = async (data) => { 
+//function to add a new employee 
+addNewEmployee = async () => {
+
+    //create db connection
+    const db = await mysql.createConnection({ host: 'localhost', user: 'root', password: '4613749J', database: 'employee_db' });
+
+    //get list of roles and converts json to appropriate format using .map()
+    const role_results = await db.query('SELECT id, title FROM role');
     
+    const list_roles = await role_results[0].map(({ id, title }) => ({ name: title, value: id }));
+
+    //get list of managers written in name value format to be called in choices inquirer prompt
+    const list_managers = [{ name: "Josh Molyneaux", value: '1' }, { name: "Lucia Molyneaux", value: '3' }, { name: "Timmy Turner", value: '6' },];
+
+    await console.log(list_managers);
+
+    const questions = [
+        {
+            type: 'input',
+            message: 'What is the employees first name?',
+            name: 'first_name',
+        },
+        {
+            type: 'input',
+            message: 'What is the employees last name?',
+            name: 'last_name',
+        },
+        {
+            type: 'list',
+            message: 'What is the employees role?',
+            name: 'employee_role',
+            choices: list_roles,
+        },
+        {
+            type: 'list',
+            message: 'Who is the employees manager?',
+            name: 'manager_name',
+            choices: list_managers,
+        },
+    ]
+
+    //Begin question prompt for add employee selection
+    const data = await inquirer.prompt(questions);
+
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+
+    const params = [data.first_name, data.last_name, data.employee_role, data.manager_name.toString()];
+
+    //runs INSERT query and then logs success message to console 
+    await db.query(sql, params);
+
+    await console.log(`Added ${data.first_name} ${data.last_name} to the database`);
+
 }
 
 module.exports = {
@@ -78,4 +175,5 @@ module.exports = {
     allEmployees,
     addDepartment,
     addNewRole,
+    addNewEmployee,
 };
