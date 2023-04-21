c6705c704ec1cb3e584f735f49a13b9cb028b429
molyneauxk93 <122243962+molyneauxk93@users.noreply.github.com>
2023-04-21 10:37:04 -0400

    Delete query.sql

--- db/query.sql
+++
@@ -1,9 +0,0 @@
-SELECT T1.id, T1.first_name, T1.last_name, role.title, department.dpt_name, role.salary, CONCAT(T2.first_name, " ", T2.last_name) AS Manager
-FROM department
-INNER JOIN role
-ON role.department_id = department.id
-INNER JOIN employee T1
-ON  role.id = T1.role_id
-LEFT JOIN employee T2
-ON T1.manager_id = T2.id
-ORDER BY T1.id ASC
